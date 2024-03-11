3ae3e1da3047f99eca871a54243f29763f9b7f56
Caio Mendes <caio.mendes@autocom-mg.com.br>
2024-03-11 18:09:26 -0300

    Iniciado diretiva para validação da força da senha

--- src/app/directives/credentials-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CredentialsValidatorDirective } from './credentials-validator.directive';
-
-describe('CredentialsValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CredentialsValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
